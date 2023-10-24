02f6143265059237dfa0cc415286f4130dad4f53
arghyawning <arghya.roy@research.iiit.ac.in>
2023-10-25 04:47:58 +0530

    search functionality and refetching added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -15,7 +15,7 @@ import { icons, SIZES } from "../../../constants";
 
 const jobtypes = ["full time", "part time", "internship", "freelance"];
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
   const [activejobtype, setactivejobtype] = useState("full time");
   return (
@@ -28,12 +28,12 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
             placeholder="What're you looking for?"
           />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
             resizeMode="contain"
